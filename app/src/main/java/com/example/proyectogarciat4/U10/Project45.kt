4e3ebf4b60763893f4764236adb1488047c4cf08
amali <alugar1951@ieselcaminas.oreg>
2023-10-23 13:45:20 +0200

    Casa

--- app/src/main/java/com/example/proyectogarciat4/U10/Project45.kt
+++ app/src/main/java/com/example/proyectogarciat4/U10/Project45.kt
@@ -1,8 +1,191 @@
 package com.example.proyectogarciat4.U10
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proyectogarciat4.ui.theme.Blue20
 
+/**Add all the values you enter using the keyboard. But if you enter 9999 the program ends.**/
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Project45() {}
+fun Project45() {
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            Column(
+                modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top
+            ) {
+
+                var number by remember { mutableStateOf("") }
+                var addition by remember { mutableStateOf(0.0) }
+                var outcome by remember { mutableStateOf("If you enter 9999 the program ends") }
+
+                Row(
+                    Modifier
+                        .padding(top = 20.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                )
+                {
+                    Text(
+                        text = "Project 45",
+                        textAlign = TextAlign.Center,
+                        color = Blue20,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+
+                OutlinedTextField(
+                    value = number,
+                    onValueChange = { number = it },
+                    label = {
+                        Text("Number")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    singleLine = true,
+
+                    )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        onClick = {
+                            if (number.toFloatOrNull() != null) {
+                                if (number.toInt() != 9999) {
+                                    addition += number.toFloat()
+                                } else {
+                                    outcome=
+                                        if(addition < 0.0){
+                                            "Below 0 : $addition"
+                                        }else if(addition > 0.0){
+                                            "Above 0: $addition"
+                                        }else{
+                                            "0"
+                                        }
+                                }
+                            } else {
+                                outcome = "Introduce an integer"
+                            }
+                            number = ""
+                        },
+                        modifier = Modifier.padding(10.dp),
+
+                        ) {
+                        Text(text = "Add")
+                    }
+                }
+                Text(
+                    text = outcome,
+                    modifier = Modifier.padding(20.dp)
+                )
+            }
+        }
+
+        else -> {
+            Column(
+                modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top
+            ) {
+                var number by remember { mutableStateOf("") }
+                var addition by remember { mutableStateOf(0.0) }
+                var outcome by remember { mutableStateOf("If you enter 9999 the program ends") }
+
+                Row(
+                    Modifier
+                        .padding(top = 20.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                )
+                {
+                    Text(
+                        text = "Project 45",
+                        textAlign = TextAlign.Center,
+                        color = Blue20,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+
+                OutlinedTextField(
+                    value = number,
+                    onValueChange = { number = it },
+                    label = {
+                        Text("Number")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    singleLine = true,
+
+                    )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        onClick = {
+                            if (number.toFloatOrNull() != null) {
+                                if (number.toInt() != 9999) {
+                                    addition += number.toFloat()
+                                } else {
+                                    outcome=
+                                        if(addition < 0.0){
+                                            "Below 0 : $addition"
+                                        }else if(addition > 0.0){
+                                            "Above 0: $addition"
+                                        }else{
+                                            "0"
+                                        }
+                                }
+                            } else {
+                                outcome = "Introduce an integer"
+                            }
+                            number = ""
+                        },
+                        modifier = Modifier.padding(10.dp),
+
+                        ) {
+                        Text(text = "Add")
+                    }
+                }
+                Text(
+                    text = outcome,
+                    modifier = Modifier.padding(20.dp)
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/proyectogarciat4/U10/Project46.kt
+++ app/src/main/java/com/example/proyectogarciat4/U10/Project46.kt
@@ -1,8 +1,227 @@
 package com.example.proyectogarciat4.U10
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proyectogarciat4.ui.theme.Blue20
 
+/**Reads the data of a checking account and reports the balance and the sum of the credit balances**/
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Project46() {}
+fun Project46() {
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            Column(
+                modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top
+            ) {
+
+                var accountNum by remember { mutableStateOf("") }
+                var balance by remember { mutableStateOf("") }
+                val listBalance by remember { mutableStateOf(mutableListOf<Pair<Int, Float>>()) }
+                var total by remember { mutableStateOf("") }
+                var outcome by remember { mutableStateOf("") }
+
+                Row(
+                    Modifier
+                        .padding(top = 20.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                )
+                {
+                    Text(
+                        text = "Project 46",
+                        textAlign = TextAlign.Center,
+                        color = Blue20,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+
+                OutlinedTextField(
+                    value = accountNum,
+                    onValueChange = { accountNum = it },
+                    label = {
+                        Text("Number")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    singleLine = true,
+
+                    )
+
+                OutlinedTextField(
+                    value = balance,
+                    onValueChange = { balance = it },
+                    label = {
+                        Text("Balance")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    singleLine = true,
+                    )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        onClick = {
+                            if (balance.toFloatOrNull() != null && accountNum.toIntOrNull() != null) {
+                                for (b in listBalance){
+                                    if (accountNum.toInt() == b.first) {
+                                        total = b.second.toString()
+                                    }
+                                    total += balance.toFloat()
+                                    listBalance.add(Pair(accountNum.toInt(), total.toFloat()))
+                                    outcome = if (total.toFloat() < 0.0) {
+                                        "Debt Balance"
+                                    } else if (total.toFloat() > 0.0) {
+                                        "Credit balance"
+                                    } else {
+                                        "Zero balance"
+                                    }
+                                }
+                                balance = ""
+                            } else {
+                                outcome = "Introduce correct parameters"
+                            }
+                            accountNum = ""
+                            balance = ""
+                        },
+                        modifier = Modifier.padding(10.dp),
+
+                        ) {
+                        Text(text = "Check")
+                    }
+                }
+                Text(
+                    text = outcome,
+                    modifier = Modifier.padding(20.dp)
+                )
+            }
+        }
+
+        else -> {
+            Column(
+                modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top
+            ) {
+                var accountNum by remember { mutableStateOf("") }
+                var balance by remember { mutableStateOf("") }
+                val listBalance by remember { mutableStateOf(mutableListOf<Pair<Int, Float>>()) }
+                var total by remember { mutableStateOf("") }
+                var outcome by remember { mutableStateOf("") }
+
+                Row(
+                    Modifier
+                        .padding(top = 20.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                )
+                {
+                    Text(
+                        text = "Project 46",
+                        textAlign = TextAlign.Center,
+                        color = Blue20,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+
+                OutlinedTextField(
+                    value = accountNum,
+                    onValueChange = { accountNum = it },
+                    label = {
+                        Text("Number")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    singleLine = true,
+
+                    )
+
+                OutlinedTextField(
+                    value = balance,
+                    onValueChange = { balance = it },
+                    label = {
+                        Text("Balance")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    singleLine = true,
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        onClick = {
+                            if (balance.toFloatOrNull() != null && accountNum.toIntOrNull() != null) {
+                                for (b in listBalance){
+                                    if (accountNum.toInt() == b.first) {
+                                        total = b.second.toString()
+                                    }
+                                    total += balance.toFloat()
+                                    listBalance.add(Pair(accountNum.toInt(), total.toFloat()))
+                                    outcome = if (total.toFloat() < 0.0) {
+                                        "Debt Balance"
+                                    } else if (total.toFloat() > 0.0) {
+                                        "Credit balance"
+                                    } else {
+                                        "Zero balance"
+                                    }
+                                }
+                                balance = ""
+                            } else {
+                                outcome = "Introduce correct parameters"
+                            }
+                            accountNum = ""
+                            balance = ""
+                        },
+                        modifier = Modifier.padding(10.dp),
+
+                        ) {
+                        Text(text = "Check")
+                    }
+                }
+                Text(
+                    text = outcome,
+                    modifier = Modifier.padding(20.dp)
+                )
+            }
+        }
+    }
+}
